b9c7a79afc7f74014d84a26ae31384266e4df7f5
Mylloon <kennel.anri@tutanota.com>
2022-08-12 19:55:28 +0200

    load html file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("cal8tor");
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //let url = "https://informatique.up8.edu/licence-iv/edt/l3.html";
+
+    // Get raw html
+    //let html = reqwest::get(url).await?.text().await?;
+    let html = include_str!("../target/debug.html");
+
+    // Parse document
+    let document = Html::parse_document(&html);
+
+    // Find the timetable
+    let selector = Selector::parse("table").unwrap();
+    let raw_timetable = document.select(&selector);
+
+    println!("{:#?}", raw_timetable);
+
+    Ok(())
 }

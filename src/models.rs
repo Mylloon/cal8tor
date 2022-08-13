9f2b775601be1efac4916ffcd7bf6d290b3a81c0
Mylloon <kennel.anri@tutanota.com>
2022-08-13 15:00:22 +0200

    add day struct

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,10 @@
+#[derive(Debug)]
 pub struct Course {
     /// Professor's name
     pub professor: String,
 
     /// List of rooms where the course takes place
-    pub room: Box<String>,
+    pub room: Vec<String>,
 
     /// Time the course starts, as a number :
     /// - 0 => first possible class of the day
@@ -13,4 +14,11 @@ pub struct Course {
 
     /// Number of time slots the course takes up in the timetable
     pub size: i8,
+}
+#[derive(Debug)]
+pub struct Day {
+    /// Day's name
+    pub name: String,
+    /// Ordered list of all the courses of the day
+    pub courses: Vec<Option<Course>>,
 }

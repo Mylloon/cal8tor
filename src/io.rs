d31a051217bf80ba7da57f4d4f7869d64b7aff96
Mylloon <kennel.anri@tutanota.com>
2022-08-15 19:41:52 +0200

    wip ics

--- src/io.rs
+++ src/io.rs
@@ -1,13 +1,19 @@
-use ics::ICalendar;
+use ics::{Event, ICalendar};
 
-pub fn export(
-    timetable: Vec<crate::timetable::models::Day>,
-    dates: std::collections::HashMap<
-        usize,
-        Vec<(chrono::DateTime<chrono::Utc>, chrono::DateTime<chrono::Utc>)>,
-    >,
-) -> ICalendar<'static> {
+type T = Vec<crate::timetable::models::Day>;
+type D = std::collections::HashMap<
+    usize,
+    Vec<(chrono::DateTime<chrono::Utc>, chrono::DateTime<chrono::Utc>)>,
+>;
+
+pub fn export(_timetable: T, _dates: D) -> ICalendar<'static> {
     let mut calendar = ICalendar::new("2.0", "cal8tor");
+
+    let event = Event::new("uid", "dtstamp");
+
+    calendar.add_event(event);
+
+    calendar.save_file("target/debug2.ics").unwrap();
 
     calendar
 }

bbe73a029c564529ae9607eecc1ea41e72db1fbb
Mylloon <kennel.anri@tutanota.com>
2022-08-15 19:20:10 +0200

    call io file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod info;
+mod io;
 mod timetable;
 
 #[tokio::main]
 async fn main() {
-    let _timetable = timetable::timetable(3, 1, None).await;
+    let timetable = timetable::timetable(3, 1, None).await;
 
-    let _info = info::info().await;
+    let info = info::info().await;
+
+    io::export(timetable, info);
 }
